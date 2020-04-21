73c79eb12c5d8d6afe7fb3714ac6910b4871ad22
Raphael M <visualight@users.noreply.github.com>
2020-04-21 22:38:17 +0200

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,27 +1,29 @@
 /**
-	 * Append automatically CSRF protection to $.POST
-	 * @type {jQuery}
-	 */
-	var csrf_token = $('meta[name="csrf-token"]').attr('content');
-	$.ajaxPrefilter(function(options, originalOptions, jqXHR){
-		if (options.type.toLowerCase() === "post") {
-			// initialize `data` to empty string if it does not exist
-			options.data = options.data || "";
+* Append automatically CSRF protection to $.POST
+* @type {jQuery}
+*/
 
-			// add leading ampersand if `data` is non-empty
-			options.data += options.data?"&":"";
+var csrf_token = $('meta[name="csrf-token"]').attr('content');
 
-			// add _token entry
-			options.data += "_token=" + encodeURIComponent(csrf_token);
-		}
-	});
+$.ajaxPrefilter(function(options, originalOptions, jqXHR){
+	if (options.type.toLowerCase() === "post") {
+		// initialize `data` to empty string if it does not exist
+		options.data = options.data || "";
 
-	/**
-	 * Get refresh CSRF token
-	 * @param callback
-	 */
-	function refreshToken(callback){
-		$.get('/api/refresh-csrf').done(function(data){
-			callback(true);
-		});
+		// add leading ampersand if `data` is non-empty
+		options.data += options.data?"&":"";
+
+		// add _token entry
+		options.data += "_token=" + encodeURIComponent(csrf_token);
 	}
+});
+
+/**
+* Get refresh CSRF token
+* @param callback
+*/
+function refreshToken(callback){
+	$.get('/refresh-csrf').done(function(data){
+		callback(true);
+	});
+}

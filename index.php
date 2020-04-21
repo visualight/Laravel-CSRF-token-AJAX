583b84a203e9198afd6c4882404a477c379ed293
Raphael M <visualight@users.noreply.github.com>
2020-04-21 22:48:37 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    ...
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    ...
+  </head>
+  <body>
+    ...
+  </body>
+</html>

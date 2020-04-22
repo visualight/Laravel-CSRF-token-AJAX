7cd3a3e4e8164b93ed317e65934ade1f15ec5f80
Raphael M <visualight@users.noreply.github.com>
2020-04-22 10:14:59 +0200

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,29 +1,27 @@
 /**
-* Append automatically CSRF protection to $.POST
-* @type {jQuery}
+* Init CSRF protection to AJAX
 */
+setHeader($('meta[name="csrf-token"]').attr('content'));
 
-var csrf_token = $('meta[name="csrf-token"]').attr('content');
-
-$.ajaxPrefilter(function(options, originalOptions, jqXHR){
-	if (options.type.toLowerCase() === "post") {
-		// initialize `data` to empty string if it does not exist
-		options.data = options.data || "";
-
-		// add leading ampersand if `data` is non-empty
-		options.data += options.data?"&":"";
-
-		// add _token entry
-		options.data += "_token=" + encodeURIComponent(csrf_token);
-	}
-});
+/**
+* Set Ajax header
+* @param data
+*/
+function setHeader(data){
+	$.ajaxSetup({
+		headers: {
+			'X-CSRF-TOKEN': data
+		}
+	});
+}
 
 /**
 * Get refresh CSRF token
 * @param callback
 */
 function refreshToken(callback){
-	$.get('/refresh-csrf').done(function(data){
+	$.get('/api/refresh-csrf').done(function(data){
+		setHeader(data);
 		callback(true);
 	});
 }
